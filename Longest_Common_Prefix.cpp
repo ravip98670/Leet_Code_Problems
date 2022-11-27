e7b1d43cdf11561810f0f71965f131e4d2b88c84
ravip98670 <ravispandey2002@gmail.com>
2022-11-27 21:21:44 +0530

    first commit

---
+++ Longest_Common_Prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res="";
+       
+        if(strs.size()==1){
+            return strs[0];
+        }
+
+        for(int i=0;i<strs[0].size();i++){
+            for(int j=1;j<strs.size();j++){
+                if(strs[0][i]!=strs[j][i]){
+                    return res;
+                }
+            }
+            res=res+strs[0][i];
+        }
+        return res;
+
+    }
+};
---
+++ Two_Sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> output;
+        for(int i=0;i<nums.size()-1;i++){
+        for(int j=i+1;j<nums.size();j++){
+            if(nums[i]+nums[j]==target){
+                output.push_back(i);
+                output.push_back(j);
+                break;
+            }
+        }
+    }
+    return output;
+    
+    }
+};
