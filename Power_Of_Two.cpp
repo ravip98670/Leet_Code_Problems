7a3762f4a8a2e94b6815b0e38c4fc79e476b6912
ravip98670 <ravispandey2002@gmail.com>
2022-12-01 23:53:51 +0530

    leetcode

---
+++ Power_Of_Two.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+    if(n==0)
+        return false;    
+    if(n==1)
+        return true;
+
+    if(n%2==0)
+        return isPowerOfTwo(n/2);
+    
+    return false;
+        
+        
+        
+        
+        
+    }
+};
