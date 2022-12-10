e781a0a3c2230f1c994b0ccfbd8a7aef14e72af1
ravip98670 <ravispandey2002@gmail.com>
2022-12-11 01:03:06 +0530

    leet code

--- Count_Number_of_Pairs_With_Absolute_Difference_K.cpp
+++ Count_Number_of_Pairs_With_Absolute_Difference_K.cpp
@@ -1,10 +1,14 @@
-int countKDifference(int* nums, int numsSize, int k){
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
     int l=0;
-    for(int i=0;i<numsSize-1;i++){
-        for(int j=i+1;j<numsSize;j++){
+    for(int i=0;i<nums.size()-1;i++){
+        for(int j=i+1;j<nums.size();j++){
             if(abs(nums[i]-nums[j])==k)
                 l++;
         }
     }
     return l;
-}
+
+    }
+};
