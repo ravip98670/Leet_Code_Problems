c4d7d0a55b304afafb24d8fbbef742d276f9ee8e
ravip98670 <ravispandey2002@gmail.com>
2023-03-04 23:57:25 +0530

    Mapping_Algo_Used_Without_Hash_Map_Simple_Array_Of_Size_26_Is_Used

---
+++ First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26] = {0};
+        
+        for(char c:s){
+            arr[c-'a']++;
+        }
+        
+        for(int i=0;i<s.size();i++){
+            if(arr[s[i]-'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
