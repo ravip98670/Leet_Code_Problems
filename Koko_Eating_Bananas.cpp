870e9d05da8ba2979c6d3aecb7514ca65cae2cfc
ravip98670 <ravispandey2002@gmail.com>
2023-03-09 22:38:33 +0530

    Medium_Level_Leetcode_Problem_Based_On_Binary_Search_Approach_An_Helper_function_is_Also_Used

---
+++ Koko_Eating_Bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool ispos(vector<int>& piles, int h,int k){
+        int time=0;
+        for(int i=0;i<piles.size();i++){
+            time+=piles[i]/k;
+            if(piles[i]%k>0)
+                time+=1;
+            if(time > h)
+                return false;
+        }
+        if(h < time)return false;
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxi = -1;
+        for(auto i:piles){
+            maxi = max(i,maxi);
+        }
+        int l=1,r = maxi,mid;
+        int k = -1;
+        while(l <= r){
+            mid = l + (r-l)/2;
+            if(ispos(piles,h,mid)){
+                k=mid;
+                r = mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return k;
+    }
+};
