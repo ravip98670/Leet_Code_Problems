1e095a5b091f8f2c2060088325b7ed2f142bff6a
ravip98670 <ravispandey2002@gmail.com>
2023-03-09 22:31:35 +0530

    Leetcode_Easy_Level_Problem

---
+++ Count_Square_Sum_Triples.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countTriples(int n) {
+         int count=0;
+        
+       
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                int sum=(i*i)+(j*j);
+
+                int tmp=sqrt(sum);
+                if(tmp*tmp==sum && tmp<=n)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
