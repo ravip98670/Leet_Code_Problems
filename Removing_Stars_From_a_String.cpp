bc7d0c68e265ce86993f024503041bcb0939398e
ravip98670 <ravispandey2002@gmail.com>
2023-03-14 14:42:46 +0530

    LeetCode_Medium_Level_Question_Solved_In_O(n)_time_Complexity_Brute_Force_Approach

---
+++ Removing_Stars_From_a_String.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        
+        string ans = "";
+        for(int i=0;i<s.size();i++){
+            if(s[i] != '*'){
+                ans.push_back(s[i]);
+            }
+            else
+                ans.pop_back();
+        }
+        return ans;
+    }
+};
