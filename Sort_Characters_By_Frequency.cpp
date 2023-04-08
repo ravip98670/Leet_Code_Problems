49bd59ba28de128d1fcf9055353ea37507980a04
ravip98670 <ravispandey2002@gmail.com>
2023-04-08 15:02:28 +0530

    using_comparator_function

---
+++ Sort_Characters_By_Frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   static bool cmp(pair<char,int>&a,pair<char,int>&b){
+        return a.second>b.second;
+    }
+    string frequencySort(string s) {
+        map<char,int>mp;
+        for(char ch:s)  
+            mp[ch]++;
+        vector<pair<char,int>>res;
+        for(auto it:mp) 
+            res.push_back(it);
+        
+        sort(res.begin(),res.end(),cmp);
+        string ans="";
+        
+        for(auto it:res){
+            int tmp = it.second;
+            
+            while(tmp--){
+                ans += it.first;
+            }
+        }
+        return ans;
+    }
+};
