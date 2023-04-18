1513dab8e1b81214971026e5dd5b7a89375e95d1
ravip98670 <ravispandey2002@gmail.com>
2023-04-18 09:34:18 +0530

    bit_manipulation

---
+++ Find_The_Original_Array_of_Prefix_Xor.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        
+        vector<int> ans(pref.size(),0);
+        
+        ans[0] = pref[0];
+        for(int i=1;i<pref.size();i++){
+           
+            ans[i] = pref[i] ^ pref[i-1];
+            
+        }
+        return ans;
+    }
+};
