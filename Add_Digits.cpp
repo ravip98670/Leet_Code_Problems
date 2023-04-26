764ff34624d17f6b17d615ece6a2f50596fd3713
ravip98670 <ravispandey2002@gmail.com>
2023-04-27 00:18:31 +0530

    modulus_operator_based_problrm

--- Add_Digits.cpp
+++ Add_Digits.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int sum=0;
+        // int sum=0;
         
-        if(num>=0 && num<=9){
-            return num;
-        }
+        // if(num>=0 && num<=9){
+        //     return num;
+        // }
         
-        while(num!=0){
-            int rem = num%10;
-            sum += rem;
-            num /= 10;
-        }
+        // while(num!=0){
+        //     int rem = num%10;
+        //     sum += rem;
+        //     num /= 10;
+        // }
         
-        return addDigits(sum);
+        // return addDigits(sum);
+        if(num == 0)
+          return 0;
+        else if(num%9 == 0)
+          return 9;
+        else
+          return num%9;
     }
 };
