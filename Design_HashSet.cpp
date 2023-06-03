335fedbc05f66eb9a8d0cc03240bf73719805298
ravip98670 <ravispandey2002@gmail.com>
2023-06-03 10:49:15 +0530

    creating_hashmap_using_object_and_class

---
+++ Design_HashSet.cpp
@@ -0,0 +1,28 @@
+class MyHashSet {
+public:
+    int v[1000001]={0};
+    MyHashSet() {
+        
+        
+    }
+    
+    void add(int key) {
+        v[key]=1;
+    }
+    
+    void remove(int key) {
+        v[key]=0;
+    }
+    
+    bool contains(int key) {
+        return v[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
