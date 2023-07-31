fc0ff5d49c8d9e21110abef1aa58abbed4a28801
ravip98670 <ravispandey2002@gmail.com>
2023-07-31 21:54:53 +0530

    Solved_in_O(n)_time_and_O(1)_space_complexity

---
+++ Rotate_List.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        if(head == NULL || head->next == NULL || k == 0) return head;
+        ListNode *temp = head;
+        int len = 1;
+        while(temp->next != NULL){
+            len++;
+            temp = temp->next;
+        }
+       
+        
+        temp->next = head;
+        k = k % len;
+        k = len - k;
+        
+        temp->next = head;
+        while(k--) 
+            temp = temp->next;
+        
+        head = temp->next;
+        temp->next = NULL;
+        return head;
+    }
+};
