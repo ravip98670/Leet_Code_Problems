afdbd9ebfa492d7b46a899455bdba6226cbd2b43
ravip98670 <ravispandey2002@gmail.com>
2023-08-02 00:54:07 +0530

    using_logic_of_stack

---
+++ Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        
+        string st;
+        for(char c:s){
+            if(st.back() == c) st.pop_back();
+            else st.push_back(c);
+        }
+       return st;
+        
+    }
+};
