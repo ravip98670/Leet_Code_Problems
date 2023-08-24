2de88c69adf45bd70817a56ddbfbf7cb5c3a71f4
ravip98670 <ravispandey2002@gmail.com>
2023-08-24 19:39:31 +0530

    Using_Stack

--- Valid_Parentheses.cpp
+++ Valid_Parentheses.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        vector<char>st;
-        for( int i=0;i<s.size();i++)
-        {
-            if(s[i]=='('||s[i]=='{'||s[i]=='[')
-            {    
-                st.push_back(s[i]);
-            }else
-            {
-                if(s[i]=='}'){
-                    if(st.size()!=0&&st[st.size()-1]=='{'){
-                        st.pop_back();
-                    }else{
-                        return false;
-                    }
-                }else if(s[i]==')'){
-                    if(st.size()!=0&&st[st.size()-1]=='('){
-                        st.pop_back();
-                    }else{
-                        return false;
-                    } 
-            }else if(s[i]==']'){
-                    if(st.size()!=0&&st[st.size()-1]=='['){
-                        st.pop_back();
-                    }else{
-                        return false;
-                    }
-                }
-            }
-        }
-        if(st.size())
+      
+
+    if (s[0] == ')' || s[0] == '}' || s[0] == ']')
+        return false;
+    if(s[s.size()-1] == '(' || s[s.size()-1] == '{' || s[s.size()-1] == '[')
+        return false;
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        cout<<s[i]<<endl;
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            st.push(s[i]);
+        else if(!st.empty() && st.top() == '(' && s[i] == ')')
+             st.pop();
+        else if (!st.empty() && st.top() == '{' && s[i] == '}')
+            st.pop();
+        else if (!st.empty() && st.top() == '[' && s[i] == ']')
+            st.pop();
+        else 
             return false;
+        //cout<<st.top()<<endl;
+    }
+        if(st.size() != 0) return false;
         return true;
+   
     }
 };
