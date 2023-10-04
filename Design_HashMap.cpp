f05016f503b4e91ef5568045333b118a3391d850
ravip98670 <ravispandey2002@gmail.com>
2023-10-04 19:53:05 +0530

    Using_Array

---
+++ Design_HashMap.cpp
@@ -0,0 +1,29 @@
+class MyHashMap {
+public:
+    int arr[10000001];
+    MyHashMap() {
+        for(int i=0;i<10000001;i++){
+            arr[i] = -1;
+        }
+    }
+    
+    void put(int key, int value) {
+        arr[key] = value;
+    }
+    
+    int get(int key) {
+        return arr[key];
+    }
+    
+    void remove(int key) {
+        arr[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
